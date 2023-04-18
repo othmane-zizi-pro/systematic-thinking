b5aa60ad9e81c056d636fe60c1807fc987ab6422
othmanez314 <118020621+othmanez314@users.noreply.github.com>
2023-04-18 10:16:29 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,31 +2,56 @@ import React from 'react';
 import './App.css';
 import Tree from 'react-d3-tree';
 
-const myTreeData = [
-  {
-    name: 'HCs',
-    children: [
-      {
-        name: 'Communicating Effectively',
-        children: [
-          {
-            name: 'Using language',
-            children: [
-              {
-                name: '#audience',
-              },
-            ],
+const myTreeData = [  {    name: 'HCs',
+children: [{name: 'Communicating Effectively',
+children: [
+  {name: 'Using language',
+  children: [
+    {name: '#audience',},
+    {name: '#composition',},
+    {name: '#connotation',},
+    {name: '#organization',},
+    {name: '#professionalism',},
+    {name: '#thesis',},
+],
           },
           {
             name: 'Using nonverbal communication',
+            children: [
+              {name: '#communicationdesign',},
+              {name: '#expression',},
+              {name: '#medium',},
+              {name: '#multimedia',},
+          ],
           },
         ],
       },
       {
         name: 'Interacting Effectively',
         children: [
-          { name: 'Interacting with complex systems' },
-          { name: 'Negotiating and persuading' },
+          { name: 'Interacting with complex systems',
+          children: [
+            {name: '#complexcausality',},
+            {name: '#emergentproperties',},
+            {name: '#levelsofanalysis',},
+            {name: '#networks',},
+            {name: '#systemdynamics',},
+            {name: '#systemmapping',},
+        ], },
+          { name: 'Negotiating and persuading' ,
+          children: [
+            {name: '#carrotandstick',},
+            {name: '#confidence',},
+            {name: '#negotiate',},
+            {name: '#nudge',},
+            {name: '#persuasion',},
+        ],},
+        { name: 'Resolving ethical problems' ,
+        children: [
+          {name: '#ethicalconsiderations',},
+          {name: '#ethicalcourage',},
+          {name: '#ethicaljudgment',},
+      ],},
         ],
       },
       {
@@ -40,17 +65,20 @@ const myTreeData = [
         name: 'Thinking Critically',
         children: [
           { name: 'Evaluating claims' },
-          { name: 'Evaluating justifications',
-          children: [
-            {
-              name: '#evidencebased',
-            },
-          ],},
+          {
+            name: 'Evaluating justifications',
+            children: [
+              {
+                name: '#evidencebased',
+              },
+            ],
+          },
         ],
       },
     ],
   },
 ];
+
 
 
 function App() {
@@ -67,7 +95,7 @@ function App() {
       <Tree
         data={myTreeData}
         translate={{ x: 300, y: 300 }}
-        nodeSize={{ x: 250, y: 100 }}
+        nodeSize={{ x: 314, y: 70 }}
         separation={{ siblings: 1.5, nonSiblings: 2 }}
       />
     </div>
