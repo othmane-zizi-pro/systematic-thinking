e16569cd3bffaa5da385f9e3867a386c34c0208b
Othmane Zizi <118020621+othmane-zizi-pro@users.noreply.github.com>
2024-10-26 16:45:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -598,7 +598,7 @@ children: [
                   >
                     #modeling
                   </a>
-                ), children: {name: "Machine Learning"},
+              ), children: [{name: "Machine Learning"}],
         ],},
           { name: (
                   <a
